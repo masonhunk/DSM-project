4357bc357783141c072227b3c7d68223d9b7a2f0
mikkelmilo <mikkelmilo@users.noreply.github.com>
2017-07-05 10:24:38 +0200

    added allocation methods to memory and more tests

--- memory/mem.go
+++ memory/mem.go
@@ -1,76 +1,130 @@
 package memory
 
-import "errors"
+import (
+  "errors"
+  "math"
+)
 
-type Page struct {
-	AccessRight int
-	memory []byte
-}
-
-
-// NO_ACCESS is nil
+const NO_ACCESS byte = 0
 const READ_ONLY byte = 1
 const READ_WRITE byte = 2
 
-
 type VirtualMemory interface {
-	Read(addr int) (byte, error)
-	Write(addr int, val byte) error
-	GetRights(addr int) byte
-	SetRights(addr int, access byte)
+  Read(addr int) (byte, error)
+  Write(addr int, val byte) error
+  GetRights(addr int) byte
+  SetRights(addr int, access byte)
+  GetPageAddr(addr int) int
+  Malloc(sizeInBytes int) (int, error)
+  Free(offset, sizeInBytes int) error
 }
 
 type Vmem struct {
-	Stack     []byte
-	AccessMap map[int]byte
-	PAGE_BYTESIZE int
+  Stack         []byte
+  AccessMap     map[int]byte
+  PAGE_BYTESIZE int
+  freeMemObjects []addrPair
 }
 
+func (m *Vmem) Free(offset, sizeInBytes int) error {
+  if offset + sizeInBytes >= len(m.Stack) {
+    return errors.New("index out of bounds")
+  }
+  foundOverlap := false
+  var newlist []addrPair
+  for _, pair := range m.freeMemObjects {
+    if pair.end < offset || pair.start > offset + sizeInBytes{
+      newlist = append(newlist, pair)
+    } else if pair.start <= offset && pair.end >= offset + sizeInBytes {
+      return nil
+    } else if pair.start <= offset && offset + sizeInBytes >= pair.end {
+      newlist = append(newlist, addrPair{min(pair.start, offset),max(pair.end, offset + sizeInBytes)})
+      foundOverlap = true
+    } else if foundOverlap && pair.start <= offset && pair.end >= offset + sizeInBytes {
+      newlist[len(newlist) - 1] = addrPair{newlist[len(newlist) - 1].start, pair.end}
+    }
+  }
+  m.freeMemObjects = newlist
+  return nil
+}
+
+type addrPair struct {
+  start, end int
+}
+
+func (m *Vmem) Malloc(sizeInBytes int) (int, error) {
+  for i, pair := range m.freeMemObjects{
+    if pair.end - pair.start + 1 >= sizeInBytes {
+      m.freeMemObjects[i] = addrPair{pair.start + sizeInBytes, pair.end}
+      return pair.start, nil
+    }
+  }
+  return 0, errors.New("insufficient space")
+}
+
+
 func NewVmem(memSize int, pageByteSize int) *Vmem {
-	m := new(Vmem)
-	m.Stack = make([]byte, memSize)
-	m.AccessMap = make(map[int]byte)
-	m.PAGE_BYTESIZE = pageByteSize
-	return m
+  m := new(Vmem)
+  m.Stack = make([]byte, memSize)
+  m.AccessMap = make(map[int]byte)
+  m.PAGE_BYTESIZE = pageByteSize
+  m.freeMemObjects = make([]addrPair, 1)
+  m.freeMemObjects[0] = addrPair{0, memSize - 1}
+  return m
 }
 
 func (m *Vmem) Read(addr int) (byte, error) {
-	access := m.AccessMap[m.getPageAddr(addr)]
-	switch access {
-	case 0:
-		return 127, errors.New("access denied")
-	case READ_ONLY:
-		return m.Stack[addr], nil
-	case READ_WRITE:
-		return m.Stack[addr], nil
-	}
-	return 127, errors.New("unknown error")
+  access := m.AccessMap[m.GetPageAddr(addr)]
+  switch access {
+  case NO_ACCESS:
+	return 127, errors.New("access denied")
+  case READ_ONLY:
+	return m.Stack[addr], nil
+  case READ_WRITE:
+	return m.Stack[addr], nil
+  }
+  return 127, errors.New("unknown error")
 }
 
 func (m *Vmem) Write(addr int, val byte) error {
-	access := m.AccessMap[m.getPageAddr(addr)]
-	switch access {
-	case 0:
-		return errors.New("access denied")
-	case READ_ONLY:
-		return errors.New("access denied")
-	case READ_WRITE:
-		m.Stack[addr] = val
-		return nil
-	}
-	return errors.New("unknown error")
+  access := m.AccessMap[m.GetPageAddr(addr)]
+  switch access {
+  case NO_ACCESS:
+	return errors.New("access denied")
+  case READ_ONLY:
+	return errors.New("access denied")
+  case READ_WRITE:
+	m.Stack[addr] = val
+	return nil
+  }
+  return errors.New("unknown error")
 
 }
 
 func (m *Vmem) GetRights(addr int) byte {
-	return m.AccessMap[m.getPageAddr(addr)]
+  return m.AccessMap[m.GetPageAddr(addr)]
 }
 
 func (m *Vmem) SetRights(addr int, access byte) {
-	m.AccessMap[m.getPageAddr(addr)] = access
+  m.AccessMap[m.GetPageAddr(addr)] = access
+}
+
+func (m *Vmem) GetPageAddr(addr int) int {
+  return addr - addr%m.PAGE_BYTESIZE
 }
 
 
-func (m *Vmem) getPageAddr(addr int) int {
-	return addr - addr % m.PAGE_BYTESIZE
+func min(a, b int) int {
+  if a < b {
+    return a
+  }
+  return b
+}
+
+
+func max(a, b int) int {
+  if a < b {
+    return b
+  }
+  return a
 }
--- tests/virtualMemory_test.go
+++ tests/virtualMemory_test.go
@@ -1,22 +1,38 @@
 package tests
 
 import (
-	"testing"
-	"DSM-project/memory"
-	"fmt"
+  "testing"
+  "DSM-project/memory"
 )
 
 func TestNoAccess(t *testing.T) {
-	mem := memory.NewVmem(4096, 128)
+  mem := memory.NewVmem(4096, 128)
 
-	r := mem.GetRights(125)
-	fmt.Println(r)
+  if r := mem.GetRights(125); r != 0 {
+	t.Error("Expected NO_ACCESS rights (= 0), got:", r)
+  }
+  if _, err := mem.Read(125); err == nil {
+	t.Error("Expected no access (nil)")
+  }
+  if err := mem.Write(125, 90); err == nil {
+	t.Error("Expected no access (nil)")
+  }
 
-	if _, err := mem.Read(125); err == nil {
-		t.Error("Expected no access (nil)")
-	}
-	if err := mem.Write(125, 90); err == nil {
-		t.Error("Expected no access (nil)")
-	}
+}
 
+func TestCorrectPageAddr(t *testing.T) {
+  mem := memory.NewVmem(4096, 128)
+  if addr := mem.GetPageAddr(57); addr != 0 {
+	t.Error("Expected address 0, got:", addr)
+  }
+  if addr := mem.GetPageAddr(1029); addr != 1024 {
+	t.Error("Expected address 1024, got:", addr)
+  }
+  if addr := mem.GetPageAddr(5051); addr != 128*39 {
+	t.Error("Expected address 4992, got:", addr)
+  }
+}
+
+func TestMalloc(t *testing.T) {
+  
 }
