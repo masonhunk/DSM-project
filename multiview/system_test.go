8458b751bf8c140f6f5f5809e4e62fc86f2186b0
Morten <m261087@gmail.com>
2017-07-13 15:52:17 +0200

    Fixed system_test

--- multiview/system_test.go
+++ multiview/system_test.go
@@ -7,33 +7,34 @@ import (
 )
 
 func TestInitialize(t *testing.T) {
-	Initialize(4096, 128)
-	ptr, err := mem.Malloc(1000)
+	mw := NewMultiView()
+	mw.Initialize(4096, 128)
+	ptr, err := mw.Malloc(1000)
 	fmt.Println("hello")
 	assert.Nil(t, err)
 	fmt.Println(ptr)
-	mem.Write(ptr, byte(9))
-	val, err := mem.Read(ptr)
+	mw.Write(ptr, byte(9))
+	val, err := mw.mem.Read(ptr)
 	fmt.Println(val, err)
-	mem.Write(ptr, byte(8))
-	val, err = mem.Read(ptr)
+	mw.Write(ptr, byte(8))
+	val, err = mw.mem.Read(ptr)
 	fmt.Println(val, err)
-
-	Shutdown()
+	mw.Shutdown()
 }
 
 func TestMalloc(t *testing.T) {
-	Initialize(4096, 128)
-	ptr, err := mem.Malloc(100)
-	_, err2 := mem.Malloc(200)
+	mw := NewMultiView()
+	mw.Initialize(4096, 128)
+	ptr, err := mw.Malloc(100)
+	_, err2 := mw.Malloc(200)
 	assert.Nil(t, err)
 	assert.Nil(t, err2)
-	err = mem.Free(ptr, 100)
+	err = mw.Free(ptr, 100)
 	assert.Nil(t, err)
-	ptr3, err3 := mem.Malloc(100)
+	ptr3, err3 := mw.Malloc(100)
 	assert.Nil(t, err3)
 	assert.Equal(t,ptr, ptr3)
-	Shutdown()
+	mw.Shutdown()
 }
 
 func TestREADWRITE(t *testing.T) {
