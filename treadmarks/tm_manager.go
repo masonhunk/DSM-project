39ea6e2bc8142d7a67d79021cb9bca418adf4799
Morten Jensen <m261087@gmail.com>
2017-07-17 17:33:06 +0200

    Started implementing the actual manager and adding test cases.

--- treadmarks/tm_manager.go
+++ treadmarks/tm_manager.go
@@ -3,14 +3,13 @@ package treadmarks
 import (
 	"DSM-project/network"
 	"errors"
-	"net"
 	"sync"
 )
 
 //Interfaces
 type LockManager interface {
-	HandleLockAcquire(id int)
-	HandleLockRelease(id int) error
+	HandleLockAcquire(id int) byte
+	HandleLockRelease(id int, newOwner byte) error
 }
 
 type BarrierManager interface {
@@ -20,7 +19,7 @@ type BarrierManager interface {
 //Lock manager implementation
 type LockManagerImp struct {
 	locks   map[int]*sync.Mutex
-	history map[int]int
+	last    map[int]byte
 	*sync.Mutex
 }
 
@@ -28,10 +27,11 @@ func NewLockManagerImp() *LockManagerImp {
 	lm := new(LockManagerImp)
 	lm.locks = make(map[int]*sync.Mutex)
 	lm.Mutex = new(sync.Mutex)
+	lm.last = make(map[int]byte)
 	return lm
 }
 
-func (lm *LockManagerImp) HandleLockAcquire(id int) {
+func (lm *LockManagerImp) HandleLockAcquire(id int) byte{
 	lm.Lock()
 	lock, ok := lm.locks[id]
 	if ok == false {
@@ -40,15 +40,17 @@ func (lm *LockManagerImp) HandleLockAcquire(id int) {
 	}
 	lm.Unlock()
 	lock.Lock()
+	return lm.last[id]
 }
 
-func (lm *LockManagerImp) HandleLockRelease(id int) error {
+func (lm *LockManagerImp) HandleLockRelease(id int, newOwner byte) error {
 	lm.Lock()
 	lock, ok := lm.locks[id]
 	if ok == false {
 		return errors.New("LockManager doesn't have a lock with ID " + string(id))
 	}
 	lm.Unlock()
+	lm.last[id] = newOwner
 	lock.Unlock()
 	return nil
 }
@@ -88,22 +90,54 @@ type tm_Manager struct {
 	network.ITransciever //embedded type
 }
 
-func NewTM_Manager(bm BarrierManager, lm LockManager) *tm_Manager {
+func NewTM_Manager(tr network.ITransciever, bm BarrierManager, lm LockManager) *tm_Manager {
 	m := new(tm_Manager)
 	m.BarrierManager = bm
 	m.LockManager = lm
+	m.ITransciever = tr
 	return m
 }
 
-func (m *tm_Manager) Connect(addr string) error {
-	conn, err := net.Dial("tcp", addr)
-	if err != nil {
-		return err
+func (m *tm_Manager) HandleMessage(message network.Message) error{
+	msg, ok := message.(TM_Message)
+	if !ok {
+		return errors.New("Could not convert message!")
 	}
-	m.ITransciever = network.NewTransciever(conn, func(message network.Message) error {
+	response := TM_Message{}
+	switch msg.Type {
+	case LOCK_ACQUIRE_REQUEST:
+		response = m.handleLockAcquireRequest(msg)
+	case LOCK_RELEASE:
+		m.handleLockReleaseRequest(msg)
+	case BARRIER_REQUEST:
+		response = m.handleBarrierRequest(msg)
+	case MALLOC_REQUEST:
+		panic("Implement me!")
+	case FREE_REQUEST:
+		panic("Implement me!")
+	}
 
-		return nil
-	})
-
+	if response.Type == ""{
+		m.ITransciever.Send(response)
+	}
 	return nil
+}
+
+func (m *tm_Manager) handleLockAcquireRequest(message TM_Message) TM_Message {
+	id := message.Id
+	message.To = m.HandleLockAcquire(id)
+	return message
+}
+
+func (m *tm_Manager) handleLockReleaseRequest(message TM_Message) {
+	id := message.Id
+	m.HandleLockRelease(id, message.From)
+}
+
+func (m *tm_Manager) handleBarrierRequest(message TM_Message) TM_Message{
+	id := message.Id
+	m.HandleBarrier(id)
+	message.To = message.From
+	message.Type = BARRIER_RESPONSE
+	return message
 }
